56dcc051f2d7f1efd9383f90c6a6aa57942b551d
Rhuan Carlos <123770612+RhuanCarlos019@users.noreply.github.com>
2023-12-14 09:51:36 -0300

    Comments

--- projeto-supermercado/app/src/main/java/sistemasupermercado/Gerenciamento/Connection/ClientesDAO.java
+++ projeto-supermercado/app/src/main/java/sistemasupermercado/Gerenciamento/Connection/ClientesDAO.java
@@ -1,74 +1,52 @@
-package sistemasupermercado.Gerenciamento.Connection;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-import sistemasupermercado.Gerenciamento.Model.Cliente;
-
+// Classe responsável pela comunicação com o banco de dados para operações relacionadas a clientes VIP
 public class ClientesDAO {
-    // Códigos para o banco de dados
-    // codigos para o banco de dados
-    // atributo
+    // Atributos
     private Connection connection;
     private List<Cliente> clientes;
 
-    // construtor
+    // Construtor
     public ClientesDAO() {
         this.connection = ConnectionFactory.getConnection();
     }
 
-    // métodos
-    // criar Tabela
+    // Método para criar a tabela no banco de dados, se não existir
     public void criaTabela() {
         String sql = "CREATE TABLE IF NOT EXISTS clientesvip_mercado (CPF VARCHAR(20) PRIMARY KEY, NOME VARCHAR(255), IDADE VARCHAR(3))";
         try (Statement stmt = this.connection.createStatement()) {
             stmt.execute(sql);
             System.out.println("Tabela criada com sucesso.");
         } catch (SQLException e) {
+            // Em caso de erro ao criar a tabela, lança uma exceção RuntimeException
             throw new RuntimeException("Erro ao criar a tabela: " + e.getMessage(), e);
         } finally {
             ConnectionFactory.closeConnection(this.connection);
         }
     }
 
-    // Listar todos os valores cadastrados
+    // Método para listar todos os clientes VIP cadastrados no banco de dados
     public List<Cliente> listarTodos() {
         PreparedStatement stmt = null;
-        // Declaração do objeto PreparedStatement para executar a consulta
         ResultSet rs = null;
-        // Declaração do objeto ResultSet para armazenar os resultados da consulta
         clientes = new ArrayList<>();
-        // Cria uma lista para armazenar os carros recuperados do banco de dados
         try {
             stmt = connection.prepareStatement("SELECT * FROM clientesvip_mercado");
-            // Prepara a consulta SQL para selecionar todos os registros da tabela
             rs = stmt.executeQuery();
-            // Executa a consulta e armazena os resultados no ResultSet
             while (rs.next()) {
-                // Para cada registro no ResultSet, cria um objeto Carros com os valores do
-                // registro
-
                 Cliente cliente = new Cliente(
                         rs.getString("cpf"),
                         rs.getString("nome"),
                         rs.getString("idade"));
-                clientes.add(cliente); // Adiciona o objeto Clientes à lista de carros
+                clientes.add(cliente);
             }
         } catch (SQLException ex) {
-            System.out.println(ex); // Em caso de erro durante a consulta, imprime o erro
+            System.out.println(ex);
         } finally {
             ConnectionFactory.closeConnection(connection, stmt, rs);
-
-            // Fecha a conexão, o PreparedStatement e o ResultSet
         }
-        return clientes; // Retorna a lista de clientes recuperados do banco de dados
+        return clientes;
     }
 
+    // Método para apagar a tabela do banco de dados
     public void apagarTabela() {
         String sql = "DROP TABLE clientesvip_mercado";
         try (Statement stmt = connection.createStatement()) {
@@ -81,9 +59,9 @@ public class ClientesDAO {
         }
     }
 
+    // Método para cadastrar um novo cliente VIP no banco de dados
     public void cadastrar(String cpf, String nomeCompleto, String idade) {
         PreparedStatement stmt = null;
-        // Define a instrução SQL parametrizada para cadastrar na tabela
         String sql = "INSERT INTO clientesvip_mercado(cpf, nome, idade) VALUES (?, ?, ?)";
         try {
             stmt = connection.prepareStatement(sql);
@@ -99,17 +77,14 @@ public class ClientesDAO {
         }
     }
 
-    // Atualizar dados no banco
+    // Método para atualizar os dados de um cliente VIP no banco de dados
     public void atualizar(String cpf, String nomeCompleto, String idade) {
         PreparedStatement stmt = null;
-        // Define a instrução SQL parametrizada para atualizar dados pela placa
         String sql = "UPDATE clientesvip_mercado SET nome = ?, idade = ? WHERE cpf = ?";
         try {
             stmt = connection.prepareStatement(sql);
             stmt.setString(1, nomeCompleto.toUpperCase().trim());
             stmt.setString(2, idade.trim());
-            ;
-            // placa é chave primaria não pode ser alterada.
             stmt.setString(3, cpf);
             stmt.executeUpdate();
             System.out.println("Dados atualizados com sucesso");
@@ -120,15 +95,14 @@ public class ClientesDAO {
         }
     }
 
-    // Apagar dados do banco
+    // Método para apagar um cliente VIP do banco de dados
     public void apagar(String cpf) {
         PreparedStatement stmt = null;
-        // Define a instrução SQL parametrizada para apagar dados pela placa
         String sql = "DELETE FROM clientesvip_mercado WHERE cpf = ?";
         try {
             stmt = connection.prepareStatement(sql);
             stmt.setString(1, cpf);
-            stmt.executeUpdate(); // Executa a instrução SQL
+            stmt.executeUpdate();
             System.out.println("Dado apagado com sucesso");
         } catch (SQLException e) {
             throw new RuntimeException("Erro ao apagar dados no banco de dados.", e);
