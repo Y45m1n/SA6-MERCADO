b2083b40d15bcec2e3de80700b9bf5036fba8c23
Rhuan Carlos <123770612+RhuanCarlos019@users.noreply.github.com>
2023-12-14 09:19:20 -0300

    Coments

--- projeto-supermercado/app/src/main/java/sistemasupermercado/Gerenciamento/View/VendasPanel.java
+++ projeto-supermercado/app/src/main/java/sistemasupermercado/Gerenciamento/View/VendasPanel.java
@@ -1,3 +1,4 @@
+// Importação de bibliotecas necessárias para a construção da interface gráfica e manipulação de dados
 package sistemasupermercado.Gerenciamento.View;
 
 import javax.swing.*;
@@ -6,48 +7,49 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
+// Importação de classes específicas do projeto relacionadas ao banco de dados, modelo e controle
 import sistemasupermercado.Gerenciamento.Connection.VendasDAO;
 import sistemasupermercado.Gerenciamento.Model.Venda;
 import sistemasupermercado.Gerenciamento.Control.VendasControl;
 
+// Classe que representa um painel para exibição e gerenciamento de vendas em uma interface gráfica
 public class VendasPanel extends JPanel {
 
-    
-    private DefaultTableModel tableModel;
-    private JTable table;
-    private List<Venda> vendas = new ArrayList<>();
-    private JScrollPane scrollPane;
+    // Declaração de variáveis de instância
+    private DefaultTableModel tableModel;  // Modelo da tabela
+    private JTable table;  // Componente de tabela
+    private List<Venda> vendas = new ArrayList<>();  // Lista de objetos Venda
+    private JScrollPane scrollPane;  // Componente de rolagem para a tabela
 
+    // Construtor da classe
     public VendasPanel() {
-        super();
+        super();  // Chama o construtor da superclasse JPanel
 
-        setLayout(new BorderLayout());
-      
+        setLayout(new BorderLayout());  // Define o layout do painel como BorderLayout
 
-        // Panel de entrada de dados
+        // Cria um painel para entrada de dados (não utilizado neste código)
         JPanel inputPanel = new JPanel(new GridLayout(5, 2, 10, 10));
-       
 
-    
-
-        // Tabela
+        // Configuração da tabela com um modelo padrão e colunas específicas
         tableModel = new DefaultTableModel(new Object[][]{}, new String[]{"id", "Cliente (CPF)", "Total", "Quantidade", "Data"});
         table = new JTable(tableModel);
-        scrollPane = new JScrollPane(table);
+        scrollPane = new JScrollPane(table);  // Adiciona a tabela a um componente de rolagem
 
-        // Adicionando os componentes ao painel principal
+        // Adiciona os componentes ao painel principal usando o layout BorderLayout
         add(inputPanel, BorderLayout.NORTH);
         add(scrollPane, BorderLayout.CENTER);
-      
 
+        // Cria a tabela no banco de dados e atualiza os dados na tabela da interface gráfica
         new VendasDAO().criaTabela();
         atualizarTabela();
-       
     }
 
+    // Método para atualizar os dados na tabela da interface gráfica
     public void atualizarTabela() {
-        tableModel.setRowCount(0);
-        vendas = new VendasDAO().listarTodos();
+        tableModel.setRowCount(0);  // Limpa todas as linhas existentes na tabela
+        vendas = new VendasDAO().listarTodos();  // Obtém a lista de todas as vendas do banco de dados
+
+        // Adiciona cada venda como uma nova linha na tabela
         for (Venda venda : vendas) {
             tableModel.addRow(new Object[]{venda.getId(), venda.getCliente(), venda.getValor(),
                     venda.getQuantidadeDeProdutos(), venda.getData()});
